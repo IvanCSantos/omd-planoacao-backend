94389f5cd3552bf5bb34e538f0b588f103b30f1a
Ivan Santos <ivancsantos@hotmail.com.br>
2025-06-19 18:14:34 -0300

    Initial project

---
+++ src/main/java/dev/ivansantos/omd_plano_de_acao/OmdPlanoDeAcaoApplication.java
@@ -0,0 +1,13 @@
+package dev.ivansantos.omd_plano_de_acao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmdPlanoDeAcaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmdPlanoDeAcaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ivansantos/omd_plano_de_acao/OmdPlanoDeAcaoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ivansantos.omd_plano_de_acao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmdPlanoDeAcaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
