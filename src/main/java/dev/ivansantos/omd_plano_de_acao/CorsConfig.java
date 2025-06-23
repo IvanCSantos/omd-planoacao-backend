7974135d509ecdac57f8320a987dc973a41cce94
Ivan Santos <ivancsantos@hotmail.com.br>
2025-06-23 15:15:03 -0300

    fix: adicionar suporte a cors localhost e ambiente de producao

---
+++ src/main/java/dev/ivansantos/omd_plano_de_acao/CorsConfig.java
@@ -0,0 +1,28 @@
+package dev.ivansantos.omd_plano_de_acao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://localhost:3000",
+                        "http://omdplanodeacao.portfolio.ivansantos.dev",
+                        "https://omdplanodeacao.portfolio.ivansantos.dev")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+      }
+    };
+  }
+}
